01061c58ba9c93e29055c3ca05ccbc81855a671c
kirti <agraharik233@gmail.com>
2026-07-19 06:08:58 -0700

    Initial Employee API

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace employeeapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            return Ok(new[]
+            {
+                new { Id = 1, Name = "John" },
+                new { Id = 2, Name = "Alice" }
+            });
+        }
+    }
+}
